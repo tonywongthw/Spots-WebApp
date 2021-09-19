68b7bbc3a350d4641e2f846e2bbdf079d8373fd2
Tony Wong <tonywong.thw@gmail.com>
2021-09-19 19:55:03 +1000

    can take input to post

--- geoApp/models.py
+++ geoApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.first_name
--- geoApp/views.py
+++ geoApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .forms import CustomerForm
 import os
 import folium
 import pandas as pd
@@ -6,6 +7,9 @@ import pandas as pd
 
 # Create your views here.
 def home(request):
+    form = CustomerForm()
+
+
     shp_dir = os.path.join(os.getcwd(), 'media', 'shp')
     # folium
     """m = folium.Map(location=[-16.22, -71.59], zoom_start=10)
@@ -17,9 +21,14 @@ def home(request):
     folium.GeoJson(os.path.join(shp_dir, 'rivers.geojson'), name='rivers',
                    style_function=lambda x: style_rivers).add_to(m)
     folium.LayerControl().add_to(m)"""
-    a = 2;
 
-    if a==1:
+    m = folium.Map(
+        location=[13.133932, 16.1039387],
+        zoom_start=2)
+
+    a=1
+    if request.method == 'POST' and a==1:
+        print(request.POST)
         cities = pd.read_csv(os.path.join(shp_dir, 'cities1.csv'))
         m = folium.Map(
             location=[43.6561, -79.3801],
@@ -29,7 +38,7 @@ def home(request):
                 location=[city['latitude'], city['longitude']],
                 tooltip=city['name']
             ).add_to(m)
-    elif a==2:
+    elif request.method == 'POST' and a==2:
         cities = pd.read_csv(os.path.join(shp_dir, 'cities2.csv'))
         m = folium.Map(
             location=[22.28967, 114.171144327727],
@@ -46,6 +55,6 @@ def home(request):
 
     # exporting
     m = m._repr_html_()
-    context = {'my_map': m}
-    # rendering
+    context = {'my_map': m, 'form': form}
+    # rendering context=context ?
     return render(request, 'geoApp/home.html', context)
