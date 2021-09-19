c8fc11894a28827d28252afa34480c86b78a2713
Tony Wong <tonywong.thw@gmail.com>
2021-09-19 18:50:28 +1000

    all done and start taking inputs

--- geoApp/views.py
+++ geoApp/views.py
@@ -17,16 +17,32 @@ def home(request):
     folium.GeoJson(os.path.join(shp_dir, 'rivers.geojson'), name='rivers',
                    style_function=lambda x: style_rivers).add_to(m)
     folium.LayerControl().add_to(m)"""
+    a = 2;
 
-    cities = pd.read_csv(os.path.join(shp_dir, 'cities.csv'))
-    m = folium.Map(
-        location=[13.14, 16.10],
-        zoom_start=2)
-    for _, city in cities.iterrows():
-        folium.Marker(
-            location=[city['latitude'], city['longitude']],
-            tooltip=city['name']
-        ).add_to(m)
+    if a==1:
+        cities = pd.read_csv(os.path.join(shp_dir, 'cities1.csv'))
+        m = folium.Map(
+            location=[43.6561, -79.3801],
+            zoom_start=14)
+        for _, city in cities.iterrows():
+            folium.Marker(
+                location=[city['latitude'], city['longitude']],
+                tooltip=city['name']
+            ).add_to(m)
+    elif a==2:
+        cities = pd.read_csv(os.path.join(shp_dir, 'cities2.csv'))
+        m = folium.Map(
+            location=[22.28967, 114.171144327727],
+            zoom_start=13)
+        for _, city in cities.iterrows():
+            folium.Marker(
+                location=[city['latitude'], city['longitude']],
+                tooltip=city['name']
+            ).add_to(m)
+    else:
+        m = folium.Map(
+            location=[13.133932, 16.1039387],
+            zoom_start=2)
 
     # exporting
     m = m._repr_html_()
